27bf07f62ad232cc3b5f17a8404ac94ea036b4b3
Ryan <hiphubryan@gmail.com>
2016-02-09 18:08:32 +0800

    Finish activerecord-exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ActiveRecord::Base
-
+  
 end
+
+
+# require "connection_adapter"
+# class User
+#   @@connection = SqliteAdapter.new
+
+#   def initialize(attributes={})
+#     @attributes = attributes
+#   end
+
+#   def id
+#     @attributes[:id]
+#   end
+
+#   def name
+#     @attributes[:name]
+#   end
+
+#   def self.find(id)
+#     attributes = @@connection.execute("SELECT * FROM users WHERE id = #{id.to_i} LIMIT 1").first
+#     new attributes # User.new(attributes)
+#   end
+
+# end
--- lib/active_record.rb
+++ lib/active_record.rb
@@ -1,4 +1,5 @@
-require "connection_adapter"
+require "connection_adapter.rb"
+require "pry"
 
 module ActiveRecord
   class Base
@@ -9,11 +10,11 @@ module ActiveRecord
     end
 
     def table_name
-      self.class.table_name
+      self.class.table_name # Base.table_name
     end
 
     def method_missing(name, *args)
-      columns = @@connection.columns(table_name)
+      columns = @@connection.columns(table_name)  
 
       if columns.include?(name)
         @attributes[name]
@@ -23,12 +24,62 @@ module ActiveRecord
     end
 
     def self.find(id)
-      attributes = @@connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
-      new attributes
+      # attributes = @@connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+      # new attributes
+      find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+    end
+
+    def self.all
+      find_by_sql("SELECT * FROM #{table_name}")
+    end
+
+    def self.find_by_sql(sql)
+      @@connection.execute(sql).map do |attributes|
+        new attributes
+      end
     end
 
     def self.table_name
+      # User.name => "User"
       name.downcase + "s" # => "users"
     end
+
   end
 end
+
+
+
+# require "connection_adapter"
+
+# module ActiveRecord
+#   class Base
+#     @@connection = SqliteAdapter.new
+
+#     def initialize(attributes={})
+#       @attributes = attributes
+#     end
+
+#     def table_name
+#       self.class.table_name
+#     end
+
+#     def method_missing(name, *args)
+#       columns = @@connection.columns(table_name)
+
+#       if columns.include?(name)
+#         @attributes[name]
+#       else
+#         super
+#       end
+#     end
+
+#     def self.find(id)
+#       attributes = @@connection.execute("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+#       new attributes
+#     end
+
+#     def self.table_name
+#       name.downcase + "s" # => "users"
+#     end
+#   end
+# end
