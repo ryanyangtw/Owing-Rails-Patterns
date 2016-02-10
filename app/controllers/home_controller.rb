c9536724dbe09e576aee1b98f1fd88866a03d355
Ryan <hiphubryan@gmail.com>
2016-02-10 22:58:21 +0800

    Implement filtering logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  # HomeController.before_action
+  # self.before_action
+  before_action :header
+
   def index
     response.write "hi from HomeController"
+  end
+
+  def header
+    response.write "<h1>My App</h1>"
   end
 end
--- lib/action_controller.rb
+++ lib/action_controller.rb
@@ -1,9 +1,19 @@
+require 'filtering'
+
 module ActionController
-  class Base
+  class Metal
     attr_accessor :request, :response
 
     def process(action_name)
       send action_name # index
     end
+  end
+
+  class Base < Metal
+    # extend Filtering  # => Using extend to add class method
+    # method in Filtering can overwrite the mehod in Metal
+    include Filtering
+    # def self.before_action(method)
+    # end
   end
 end
