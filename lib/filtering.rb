8d20a31f0fe2107506f81b02d59f76af4e9a26ea
Ryan <hiphubryan@gmail.com>
2016-02-11 16:31:13 +0800

    Finish activesupport-concern-exercise

--- lib/filtering.rb
+++ lib/filtering.rb
@@ -1,30 +1,44 @@
+require 'active_support/concern'
+require 'active_support/callbacks'
+
 module Filtering
-  def self.included(base) # base = ActionController::Base
-    base.extend ClassMethods
+  # def self.included(base) # base = ActionController::Base
+  #   base.extend ClassMethods
+  # end
+  extend ActiveSupport::Concern
+  include ActiveSupport::Callbacks
+
+  included do
+    define_callbacks :process
   end
 
   module ClassMethods
     def before_action(method)
-      before_actions << method
+      set_callback :process, :before, method
+      # before_actions << method
     end
 
-    def before_actions
-      @before_actions ||= []
-    end
+    # def before_actions
+    #   @before_actions ||= []
+    # end
 
     def after_action(method)
-      after_actions << method
+      set_callback :process, :after, method
+      # after_actions << method
     end
 
-    def after_actions
-      @after_actions ||= []
-    end
+    # def after_actions
+    #   @after_actions ||= []
+    # end
   end
 
   def process(action_name)
-    self.class.before_actions.each { |method| send method }
-    super
-    self.class.after_actions.each { |method| send method }
+    # self.class.before_actions.each { |method| send method }
+    # super
+    # self.class.after_actions.each { |method| send method }
+    run_callbacks :process do
+      super
+    end
   end
 
 end
